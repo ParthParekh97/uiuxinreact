b5aae8cdf6c474a904a583ae4f8795714dfc03b8
Parth Parekh <parthkparekh2014@gmail.com>
2023-04-12 13:14:50 -0400

    Message

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>This Landing Page is build By Parth Parekh for Practicing React and UI Framework to showcase his sharp skills by this frontend development.</p>
+      <p>This landing page is built by Parth Parekh to showcase his sharp skills in frontend development. It is built using React and a UI framework, and it is designed to be user-friendly and visually appealing. The page features a variety of sections, including a portfolio, a blog, and a contact form. It is a great example of Parth's skills and abilities, and it is sure to impress potential employers and clients.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
